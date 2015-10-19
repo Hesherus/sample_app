aa2f97d79f2afca4b6a5eca8ad6fd74991e8a125
Hesherus <el_torrro@mail.ru>
2015-10-19 11:51:24 +0300

    Add user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,11 +11,12 @@ def current_user
   def current_user?(user)
     user == current_user
   end
-  def sign_in(user)
-    remember_token = User.new_remember_token
-    cookies.permanent[:remember_token] = remember_token
-    user.update_attribute(:remember_token, User.encrypt(remember_token))
-    self.current_user = user
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
   end
 
  
